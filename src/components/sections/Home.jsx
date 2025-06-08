3f2c7356a054560e4d8f288e56d2bbb9f9e129b3
fishrifle <163429092+fishrifle@users.noreply.github.com>
2025-06-08 03:57:10 -0500

    Fix vite base path for Vercel

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,5 @@
 
 
-
-
 export const Home = () => {
   return (
     <section
@@ -22,7 +20,7 @@ export const Home = () => {
           and modern tooling
           </p>
           <div className="flex justify-center space-x-4">
-            <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)] ">Veiw Projects</a>
+            <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)] ">View Projects</a>
 
             <a href="#contact" className=" border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10 ">Contact Me</a>
           </div>
