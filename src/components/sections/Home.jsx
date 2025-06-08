bb7ff0980e7f7bead798e04091576137472bc604
fishrifle <163429092+fishrifle@users.noreply.github.com>
2025-06-08 02:54:10 -0500

    deploy

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,4 +1,4 @@
-import { RevealOnScroll } from "../RevealOnScroll";
+
 
 
 
@@ -8,14 +8,18 @@ export const Home = () => {
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
-      <RevealOnScroll>
+      
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
             Welcome To My Portfolio
           </h1>
 
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            Motivated and adaptable Junior Web Developer with a solid foundation in front-end and back-end technologies.  Proficient in HTML, CSS, JavaScript, React, Node.js, and MongoDB, with hands-on experience building responsive web applications and integrating dynamic user interfaces with server-side infrastructures.
+          Detail-oriented Junior Frontend Developer with hands-on experience building interactive web apps
+          using React, Next.js, TypeScript, Tailwind CSS 4, and MongoDB. Strong collaborator and team lead
+          at Banyan Labs, working on production apps supporting justice-involved individuals. Experienced
+          with Agile/Scrum, sprint planning, and Git-based workflows. Passionate about clean UI, scalability,
+          and modern tooling
           </p>
           <div className="flex justify-center space-x-4">
             <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)] ">Veiw Projects</a>
@@ -23,7 +27,7 @@ export const Home = () => {
             <a href="#contact" className=" border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10 ">Contact Me</a>
           </div>
         </div>
-      </RevealOnScroll>
+      
     </section>
   );
 };
